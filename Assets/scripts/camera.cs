86b54c8d8ee43730ee72d61ff5d3c2215538f6ce
luisgtse <l183015@dac.unicamp.br>
2021-01-17 23:08:06 -0300

    first commit

---
+++ Assets/scripts/camera.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    public float rotacao = 1;
+    public Transform Target, Player;
+    float mouseX, mouseY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        ControleCamera();
+    }
+
+    void ControleCamera()
+    {
+        mouseX += Input.GetAxis("Mouse X") * rotacao;
+        mouseY -= Input.GetAxis("Mouse Y") * rotacao;
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+        transform.LookAt(Target);
+
+        Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        Player.rotation = Quaternion.Euler(0, mouseX, 0);
+    }
+}
---
+++ Assets/scripts/personagem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class personagem : MonoBehaviour
+{
+    public float velocidade;
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        MovimentoPlayer();
+    }
+
+    void MovimentoPlayer()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 movimento = new Vector3(horizontal, 0f, vertical) * velocidade * Time.deltaTime;
+        transform.Translate(movimento, Space.Self);
+
+        if(Input.GetButtonDown("Jump")){
+            rb.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+        }
+    }
+}
